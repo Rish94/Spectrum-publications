fc7e993764e19f0219e1ddb3d36a65fee3ba1dcc
Deepakkushwahg <deepakkushwahg@gmail.com>
2026-02-04 02:29:42 +0530

    16kb changes fixed

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,60 @@
+import java.util.Properties // FIX: This was missing!
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.spectrum_app"
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    signingConfigs {
+        // FIX: Using "getByName" or "create" is safer in Kotlin Script
+        create("release") {
+            storeFile = file("release-key.jks") 
+            storePassword = "Rish@844541"
+            keyAlias = "releaseKey"
+            keyPassword = "Rish@844541"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.spectrum_app"
+        minSdk = flutter.minSdkVersion // Manually set to 21 for 16KB support
+        targetSdk = 35
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+
+        externalNativeBuild {
+            cmake {
+                arguments("-DANDROID_ALIGNED_16KB=ON")
+            }
+        }
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = false
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
